1c2ab4da858990a30898be86ee43206394221005
Kent clarence Evangelista <kentclarence5368@gmail.com>
2025-12-12 17:27:36 +0800

    fixing errors on mailer

--- config/mail.php
+++ config/mail.php
@@ -38,7 +38,7 @@ return [
     'mailers' => [
 
         'smtp' => [
-            'transport' => 'smtp',
+            'transport' => 'sendgrid',
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
